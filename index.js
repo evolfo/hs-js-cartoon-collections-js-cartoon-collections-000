88e603853c814f19d66eb91edf25140c21d0f19f
evolfo <spampinatodesign@gmail.com>
2018-12-17 15:35:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function dwarfRollCall(dwarves) {
+	var dwarfList = [];
+	for (var i = 0; i < dwarves.length; i++) {
+		dwarfList.push(`${i+1}. ${dwarves[i]}`);
+	}
+	var joinedDwarves = [dwarfList.join(' ')];
+	return `${joinedDwarves.toString()} `;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+	var elements = [];
+	for (var i = 0; i < planeteerCalls.length; i++) {
+		elements.push(`${planeteerCalls[i]}!`);
+	}
+	var toUpperCase = elements.map(function(x) {return x.toUpperCase(); });
+	return toUpperCase;
 }
 
 function longPlaneteerCalls(words) {
+	for (var i = 0; i < words.length; i++) {
+		if (words[i].length > 4) {
+			return true;
+		}
+	} return false;
 }
 
 function findTheCheese (foods) {
+	var cheeses = ['gouda', 'cheddar', 'camembert'];
+	for (var i = 0; i < foods.length; i++) {
+		var foodIncludes = foods.includes(cheeses[i])
+			if (foodIncludes === true) {
+			return cheeses[i];
+			}
+		} return 'no cheese!';
 }
